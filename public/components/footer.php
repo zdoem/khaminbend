c607bb49803763087c81de194f4dbfbd0f76ee2c
dev-pong <nanayrx@gmail.com>
2020-12-13 11:48:50 +0700

    fix footer

--- public/components/footer.php
+++ public/components/footer.php
@@ -5,11 +5,17 @@ defined('ROOT') or exit('No access allowed');
     </div>
     <!-- /.content-wrapper -->
     <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2020 BY <!--2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>. --></strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.5
+  <footer class="main-footer" style="font-size: .8em;">
+        Copyright &copy; 2020
+     <strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br>
+         224 หมู่ที่ 11 ถนนประโคนชัย-กรสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250  โทรศัพท์/โทรสาร . 0-4466-6300
+     <br>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
+     <br>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
+     <div class="float-right d-none d-sm-inline-block" id="parent">
+           ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong>
+      <div id="hover-content">
+        Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
+       </div>     
     </div>
   </footer>
 
