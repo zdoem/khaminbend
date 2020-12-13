fff68506b9ccbff9d749e5e6dec0eb362f17af97
Zdoem.pwong <Zdoem.pwong@DESKTOP-JVFGISG>
2020-12-13 11:34:06 +0700

    user manager,footer

--- public/components/footerX.php
+++ public/components/footerX.php
@@ -5,22 +5,44 @@ defined('ROOT') or exit('No access allowed');
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <!-- To the right -->
+    <!-- To the right 
     <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
+      Dev Team. 
+    </div> -->
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    Copyright &copy; 2020
+     <strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br>
+         224 หมู่ที่ 11 ถนนประโคนชัย-กรสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250  โทรศัพท์/โทรสาร . 0-4466-6300
+     <br>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
+     <br>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
+     <div class="float-right d-none d-sm-inline-block" id="parent">
+           ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong>
+      <div id="hover-content">
+        Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
+       </div>     
+    </div>
+    
   </footer>
 </div>
 <!-- ./wrapper -->
 
+<style>
+<!--
+#hover-content {
+    display:none;
+}
+#parent:hover #hover-content {
+    display:block;
+}
+-->
+</style>
+
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
+<script src="assets/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 </body>
