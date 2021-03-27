51d8cede10c5d215a28803b7d1735a9c1e4599b8
dev-pong <nanayrx@gmail.com>
2021-03-27 23:11:32 +0700

    fix

--- public/components/footer.php
+++ public/components/footer.php
@@ -4,28 +4,64 @@ defined('ROOT') or exit('No access allowed');
    
     </div>
     <!-- /.content-wrapper -->
-    <!-- Main Footer -->
-  <footer class="main-footer" style="font-size: .8em;">
+  <!-- Main Footer -->
+  <footer class="main-footer row" style="font-size: .8em;">
+  	<div class="col-md-6">
         Copyright &copy; 2020
-     <strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br>
-         224 หมู่ที่ 11 ถนนประโคนชัย-กระสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250  โทรศัพท์/โทรสาร . 0-4466-6300
-     <br>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
-     <br>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
-     <div class="float-right d-none d-sm-inline-block" id="parent">
-           ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong>
-      <div id="hover-content">
-        Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
-       </div>     
-    </div>
+		<strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br/>
+         224 หมู่ที่ 11 ถนนประโคนชัย-กระสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250<br /> โทรศัพท์/โทรสาร . 0-4466-6300
+		<br/>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
+		<br/>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
+		
+	</div>
+	<div class="col-md-6 d-sm-inline-block parent-dev">
+		<span> ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong></span>
+		     
+	</div>
+	<div id="hover-content">
+			Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
+	   </div>
   </footer>
   <div style="display: none;" id="xhtml"></div>
-  <style>
+<style>
 <!--
+.main-footer{
+	padding: 2em 20px; /*top left*/
+	margin-right: 0;
+}
+.parent-dev span{
+    position: absolute;
+    right: 0px;     
+    padding: 50px 0px 10px; /*top right bottom*/
+}
+@media(max-width: 767.98px) {
+	.parent-dev span{
+		position: absolute;
+		right: 0px;    		
+		padding: 5px 0px 20px;
+	}
+	.parent-dev:hover + #hover-content {
+		display:inline-block;
+		position: absolute;
+		right: 0px;     
+		padding: 90px 0px 2px;
+	}
+}
+@media(max-width: 350px) {
+	.parent-dev span{
+		position: absolute;
+		right: 0px;    		
+		padding: 5px 0px 10px;
+	}
+}
 #hover-content {
     display:none;
 }
-#parent:hover #hover-content {
+.parent-dev:hover + #hover-content {
     display:block;
+	position: absolute;
+    right: 0px;     
+    padding: 70px 0px 2px;
 }
 -->
 </style>
--- public/components/footerX.php
+++ public/components/footerX.php
@@ -4,36 +4,67 @@ defined('ROOT') or exit('No access allowed');
    
   <div style="display: none;" id="xhtml"></div>
   <!-- Main Footer -->
-  <footer class="main-footer" style="font-size: .8em;">
-    <!-- To the right 
-    <div class="float-right d-none d-sm-inline">
-      Dev Team. 
-    </div> -->
-    <!-- Default to the left -->
-    Copyright &copy; 2020
-     <strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br>
-         224 หมู่ที่ 11 ถนนประโคนชัย-กระสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250  โทรศัพท์/โทรสาร . 0-4466-6300
-     <br>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
-     <br>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
-     <div class="float-right d-none d-sm-inline-block" id="parent">
-           ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong>
-      <div id="hover-content">
-        Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
-       </div>     
-    </div>
-    
+  <footer class="main-footer row" style="font-size: .8em;">
+  	<div class="col-md-6">
+        Copyright &copy; 2020
+		<strong> สำนักงานเทศบาลตำบลโคกขมิ้น </strong> All rights reserved.<br/>
+         224 หมู่ที่ 11 ถนนประโคนชัย-กระสัง ตำบลโคกขมิ้น อำเภอพลับพลาชัย จังหวัดบุรีรัมย์  31250<br />  โทรศัพท์/โทรสาร . 0-4466-6300
+		<br/>FACEBOOK :<a href="https://www.facebook.com/abt.kokkhamin"> เทศบาลโคกขมิ้น </a>
+		<br/>websiet :<a href="http://www.kokkhamin.go.th"> www.kokkhamin.go.th </a>
+		
+	</div>
+	<div class="col-md-6 d-sm-inline-block parent-dev">
+		<span> ออกแบบและพัฒนาโดย : บริษัท ทีพี โกบิซ จำกัด   <strong> Version 1.0</strong></span>
+		     
+	</div>
+	<div id="hover-content">
+			Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved. Version</b> 3.0.5
+	 </div>
   </footer>
 </div>
 <!-- ./wrapper -->
 
-<style> 
+<style>
+.main-footer{
+	padding: 2em 20px;
+	margin-right: 0;
+}
+.parent-dev span{
+    position: absolute;
+    right: 0px;     
+    padding: 50px 0px 10px;
+}
+@media(max-width: 767.98px) {
+	.parent-dev span{
+		position: absolute;
+		right: 0px;    		
+		padding: 5px 0px 20px;
+	}
+	.parent-dev:hover + #hover-content {
+		display:inline-block;
+		position: absolute;
+		right: 0px;     
+		padding: 90px 0px 2px;
+	}
+}
+@media(max-width: 350px) {
+	.parent-dev span{
+		position: absolute;
+		right: 0px;    		
+		padding: 5px 0px 10px;
+	}
+}
 #hover-content {
     display:none;
 }
-#parent:hover #hover-content {
+.parent-dev:hover + #hover-content {
     display:block;
-} 
-</style> 
-<!-- REQUIRED SCRIPTS --> 
+	position: absolute;
+    right: 0px;     
+    padding: 70px 0px 2px;
+}
+</style>
+
+<!-- REQUIRED SCRIPTS -->
 </body>
 </html>
