6df250cec8de20367386d64eb0f67df2ffa43eaa
niwath <dev.niwath@gmail.com>
2021-06-21 22:02:26 +0700

    fix

--- public/handler/family/family_duplicate_mouseid.php
+++ public/handler/family/family_duplicate_mouseid.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 $house_no = trim((isset($_POST['house_no']) ? $_POST['house_no'] : ''));  
 $mooHouse = trim((isset($_POST['mooHouse']) ? $_POST['mooHouse'] : ''));  
 $house_no =rawurldecode($house_no); 
-echo json_encode(['status'=>'nodupicate']);exit();
+
 if(isset($_POST['id'])&&strlen(trim(@$_POST['id']))>0){
     $rows_old =$db::select("SELECT fam_id,d_survey,house_no,house_moo FROM  fm_fam_hd AS a 
     WHERE  house_no=? AND house_moo=? AND fam_id!=? AND YEAR(d_survey)  IN (
